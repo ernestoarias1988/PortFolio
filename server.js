b08ec72a1c6134392fb9d7cef88c492f98615c14
Ernesto Arias <ernestoarias1988@gmail.com>
2022-01-02 15:49:52 -0300

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const path = require('path');
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
+const publicPath = path.join(__dirname, '..','PortFolio', 'public');
+
 
 app.use(express.static(publicPath));
 app.get('*', (req, res) => {
