5f77b2cbf3de0f0584fec6584c40c3650918b20f
Ernesto Arias <ernestoarias1988@gmail.com>
2022-01-02 14:05:27 -0300

    upload PortFolio

--- server.js
+++ server.js
@@ -15,9 +15,9 @@ app.use("/", contactRoute);
 if(process.env.NODE_ENV === "production")
 {
     app.use(express.static("client/build"));
-    app.get("*",(req,res)=>
-        res.sendFile(path.resolve(__dirname,"client","public","index.html"))
-    )
+    app.get("*",(req,res)=> (
+        res.sendFile(path.resolve(__dirname,"client","build","index.html"))
+    ))
 }
 
 const port = process.env.PORT || 5000
