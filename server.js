9ee93a02cb3e6acd1e2aa716cd68bde4c5ab7f09
Ernesto Arias <ernestoarias1988@gmail.com>
2022-01-02 10:33:36 -0300

    Update server.js

--- server.js
+++ server.js
@@ -12,13 +12,13 @@ app.use(cors())
 
 app.use("/", contactRoute);
 
-// if(process.env.NODE_ENV === "production")
-// {
-//     app.use(express.static("client/build"));
-//     app.get("*",(req,res)=>
-//         res.sendFile(path.resolve(__dirname,"client", "build","index.html"))
-//     )
-// }
+if(process.env.NODE_ENV === "production")
+{
+    app.use(express.static("client/build"));
+    app.get("*",(req,res)=>
+        res.sendFile(path.resolve(__dirname,"client","index.html"))
+    )
+}
 
 const port = process.env.PORT || 5000
 app.listen(port, console.log(`Server started on port ${port}`));
