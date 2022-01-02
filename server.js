699d8b1b63ac49b1b90f0a82bc5e7ddc4fbf928e
Ernesto Arias <ernestoarias1988@gmail.com>
2022-01-02 10:06:14 -0300

    upload

--- server.js
+++ server.js
@@ -14,10 +14,10 @@ app.use("/", contactRoute);
 
 if(process.env.NODE_ENV === "production")
 {
-    app.use(express.static("client/build"))
-    app.get("*",(req,res)=>(
-        res.sendFile(path.resolve(__dirname,"client", "build","index.hyml"))
-    ))
+    app.use(express.static("client/build"));
+    app.get("*",(req,res)=>
+        res.sendFile(path.resolve(__dirname,"client", "build","index.html"))
+    )
 }
 
 const port = process.env.PORT || 5000
